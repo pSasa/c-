23daf2190e72e05bacd596d4a73cea221a01c816
psasa <p_sasa@mail.ru>
2010-12-15 12:27:23 +0300

    -- finish first task

---
+++ task1/CircleFigure.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Task1
+{
+    class CircleFigure: BaseFigure
+    {
+        private int m_x = 0;
+        private int m_y = 0;
+        private int m_r = 0;
+
+        public int X
+        {
+            get { return m_x; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_x = value;
+                }
+            }
+        }
+
+        public int Y
+        {
+            get { return m_y; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_y = value;
+                }
+            }
+        }
+
+        public int R
+        {
+            get { return m_r; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_r = value;
+                }
+            }
+        }
+
+        public CircleFigure(int x, int y, int r)
+        {
+            X = x;
+            Y = y;
+            R = r;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            if (g != null)
+            {
+                Pen pen = new Pen(Color.Black, 1);
+                g.DrawEllipse(pen, m_x - m_r / 2, m_y - m_r / 2, m_r, m_r);
+            }
+        }
+    }
+}
---
+++ task1/RectangleFigure.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Task1
+{
+    class RectangleFigure: BaseFigure
+    {
+        private int m_x = 0;
+        private int m_y = 0;
+        private int m_width = 0;
+        private int m_height = 0;
+
+        public int X
+        {
+            get { return m_x; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_x = value;
+                }
+            }
+        }
+
+        public int Y
+        {
+            get { return m_y; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_y = value;
+                }
+            }
+        }
+
+        public int Width
+        {
+            get { return m_width; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_width = value;
+                }
+            }
+        }
+
+        public int Height
+        {
+            get { return m_height; }
+            set
+            {
+                if (value >= 0 && value <= 1024)
+                {
+                    m_height = value;
+                }
+            }
+        }
+        public RectangleFigure(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            if (g != null)
+            {
+                Pen pen = new Pen(Color.Black, 1);
+                g.DrawRectangle(pen, m_x, m_y, m_width, m_height);
+            }
+        }
+    }
+}
