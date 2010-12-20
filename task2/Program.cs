aeed585108c42eb8f55b24ba01bb323e4ce07490
psasa <p_sasa@mail.ru>
2010-12-20 15:49:01 +0300

    -- First version of second task

---
+++ task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static int IntCompare(Object left, Object right)
+        {
+            if ((int)left == (int)right)
+            {
+                return 0;
+            }
+            else if ((int)left > (int)right)
+            {
+                return 1;
+            }
+            return -1;
+        }
+        static void PartiallyDone(Object sender, EventArgs ev)
+        {
+            Console.WriteLine("Done percent:" + ((PartiallEventArgs)ev).done);
+        }
+
+        static void ElementReplacing(Object sender, EventArgs ev)
+        {
+            Console.WriteLine("Replace elements:" + ((ReplaceEventArgs)ev).left);
+        }
+
+        static void Main(string[] args)
+        {
+            Object[] array;
+            array = new Object[100];
+            Random r = new Random();
+            int s1 = 0;
+            int s2 = 0;
+            
+            Console.WriteLine("Before");
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = r.Next(500);
+                Console.Write((int)array[i] + ",");
+                s1 += (int)array[i];
+            }
+            Compare cmp = new Compare(IntCompare);
+            ShellSort.onPartiallyDone += PartiallyDone;
+            ShellSort.onElementReplacing += ElementReplacing;
+            ShellSort.Sort(array, cmp);
+            Console.WriteLine("");
+            Console.WriteLine("After");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(array[i] + ",");
+                s2 += (int)array[i];
+            }
+            Console.WriteLine("");
+            Console.WriteLine(s1 + "=" + s2);
+        }
+    }
+}
---
+++ task2/ShellSort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public delegate int Compare(Object left, Object right);
+
+namespace Task2
+{
+    class PartiallEventArgs : EventArgs
+    {
+        public int done = 0;
+    }
+
+    class ReplaceEventArgs : EventArgs
+    {
+        public int left;
+        public int right;
+    }
+    class ShellSort
+    {
+        public static event EventHandler onPartiallyDone;
+        public static event EventHandler onElementReplacing;
+
+        public static void Sort(Object[] array, Compare cmp)
+        {
+            int shift = 0;
+            int steps = 4;
+            int len = array.Length;
+            int step = 1;
+
+            shift = (int)Math.Pow(2, steps - 1);
+
+            while (shift > 0)
+            {
+                for (int i = shift; i < len; i++)
+                {
+                    Object temp = array[i];
+                    int j;
+                    for (j = i - shift; (j >= 0) && cmp(array[j], temp) > 0; j -= shift)
+                    {
+                        if (onElementReplacing != null)
+                        {
+                            ReplaceEventArgs evArgs = new ReplaceEventArgs();
+                            evArgs.left = j;
+                            evArgs.left = j + shift;
+                            onElementReplacing(null, evArgs);
+                        }
+                        array[j + shift] = array[j];
+                    }
+                    array[j + shift] = temp;
+                }
+                if (onPartiallyDone != null)
+                {
+                    PartiallEventArgs evArgs = new PartiallEventArgs();
+                    evArgs.done = 100 * step / steps;
+                    onPartiallyDone(null, evArgs);
+                }
+                step++;
+                shift = shift >> 1;
+            }
+        }
+    }
+}
