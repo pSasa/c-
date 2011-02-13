6600c7a001c986967e2bf7423dda119606218ee3
Sasa <p_sasa@mail.ru>
2011-02-14 00:12:23 +0300

    init task5

---
+++ task5/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace task5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.image = new System.Windows.Forms.PictureBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.fileList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image
+            // 
+            this.image.Location = new System.Drawing.Point(200, 0);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(608, 462);
+            this.image.TabIndex = 0;
+            this.image.TabStop = false;
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(0, 0);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 2;
+            this.Browse.Text = "button1";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileList
+            // 
+            this.fileList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.fileList.FormattingEnabled = true;
+            this.fileList.Location = new System.Drawing.Point(0, 29);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(194, 433);
+            this.fileList.TabIndex = 3;
+            this.fileList.SelectedIndexChanged += new System.EventHandler(this.fileList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 467);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.fileList);
+            this.Controls.Add(this.Browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.ListBox fileList;
+    }
+}
+
---
+++ task5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task5
+{
+    public partial class Form1 : Form
+    {
+        private string folder = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                folder = fbd.SelectedPath;
+                folder = @"c:\1\";
+                DirectoryInfo di = new DirectoryInfo(folder);
+                FileInfo[] files = di.GetFiles("*.png");
+                fileList.Items.Clear();
+                fileList.Items.AddRange(files.ToArray() );
+            }
+        }
+
+        private void fileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            image.Image = Image.FromFile(Path.Combine(folder, fileList.SelectedItem.ToString()));
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
