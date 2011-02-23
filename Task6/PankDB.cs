6975eec034a03c655b0434cc0cae778d614fcc82
Sasa <p_sasa@mail.ru>
2011-02-23 23:45:33 +0300

    6 задание. Начало

---
+++ Task6/PankDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+
+namespace Task6
+{
+    abstract class PankDB
+    {
+        protected DbConnection connection = null;
+        private DbTransaction transaction = null;
+
+        public PankDB(string url)
+        {
+
+        }
+
+        abstract string convert 
+
+        public void ExecuteNonQuery(string sql)
+        {
+            DbCommand dc = connection.CreateCommand();
+            dc.CommandText = sql;
+            dc.ExecuteNonQuery();
+        }
+
+        public DbDataReader ExecuteReader(string sql)
+        {
+            DbCommand dc = connection.CreateCommand();
+            dc.CommandText = sql;
+            DbDataReader res = dc.ExecuteReader();
+            return res;
+        }
+
+        public void BeginTransaction()
+        {
+            transaction = connection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            transaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            transaction.Rollback();
+        }
+
+
+    }
+}
---
+++ Task6/PankODBC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using System.Data.Odbc;
+
+namespace Task6
+{
+    class PankODBC : PankDB
+    {
+        public PankODBC(string url)
+            :base(url)
+        {
+            connection = new OdbcConnection(url);
+        }
+    }
+}
