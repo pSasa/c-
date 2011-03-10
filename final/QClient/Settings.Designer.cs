11d2cec746e2beec737173058cbb0b9869847d1c
Sasa <p_sasa@mail.ru>
2011-03-10 20:24:32 +0300

    добавлени диалог Settings

---
+++ final/QClient/Settings.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace QClient
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EServer = new System.Windows.Forms.TextBox();
+            this.EPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BOk = new System.Windows.Forms.Button();
+            this.BCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EServer
+            // 
+            this.EServer.Location = new System.Drawing.Point(62, 6);
+            this.EServer.Name = "EServer";
+            this.EServer.Size = new System.Drawing.Size(100, 20);
+            this.EServer.TabIndex = 0;
+            this.EServer.Text = "127.0.0.1";
+            // 
+            // EPort
+            // 
+            this.EPort.Location = new System.Drawing.Point(62, 39);
+            this.EPort.Name = "EPort";
+            this.EPort.Size = new System.Drawing.Size(100, 20);
+            this.EPort.TabIndex = 1;
+            this.EPort.Text = "8888";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сервер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Порт";
+            // 
+            // BOk
+            // 
+            this.BOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.BOk.Location = new System.Drawing.Point(194, 3);
+            this.BOk.Name = "BOk";
+            this.BOk.Size = new System.Drawing.Size(75, 23);
+            this.BOk.TabIndex = 4;
+            this.BOk.Text = "Сохранить";
+            this.BOk.UseVisualStyleBackColor = true;
+            // 
+            // BCancel
+            // 
+            this.BCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BCancel.Location = new System.Drawing.Point(194, 36);
+            this.BCancel.Name = "BCancel";
+            this.BCancel.Size = new System.Drawing.Size(75, 23);
+            this.BCancel.TabIndex = 5;
+            this.BCancel.Text = "Отмена";
+            this.BCancel.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BCancel;
+            this.ClientSize = new System.Drawing.Size(286, 79);
+            this.Controls.Add(this.BCancel);
+            this.Controls.Add(this.BOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EPort);
+            this.Controls.Add(this.EServer);
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EServer;
+        private System.Windows.Forms.TextBox EPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BOk;
+        private System.Windows.Forms.Button BCancel;
+
+    }
+}
---
+++ final/QClient/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QClient
+{
+    public partial class Settings : Form
+    {
+        public string Server
+        {
+            get { return EServer.Text; }
+            set { EServer.Text = value; }
+        }
+
+        public int Port
+        {
+            get { return Int32.Parse(EPort.Text); }
+            set { EPort.Text = value.ToString(); }
+        }
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
